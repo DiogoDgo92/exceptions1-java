9177816312786ee580d0959a20892604c319609c
Diogo Barroso <diogo.dgo.1992@gmail.com>
2024-07-24 09:46:15 -0300

    very bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -47,6 +47,7 @@ public class Program {
 			else {
 			reservation.updateDates(checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);	
+			System.out.println("Reservation: " + reservation);	
 			}
 		}					
 		sc.close();
